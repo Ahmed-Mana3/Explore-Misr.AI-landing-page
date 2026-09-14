4327df02b12d8e26277532e45a57e068164c40df
am537@Ahmed_Medhat <you@example.com>
2026-09-14 09:14:44 +0200

    Allow public access to manage waitlist view

--- exploremisr/views.py
+++ exploremisr/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 
 # Global list to store waitlist entries in memory
 WAITLIST_NUMBERS = []
